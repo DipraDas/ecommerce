69f9f3093cb2137a66132b8638cd14e74139b52f
Dipra <dipradas5940@gmail.com>
2023-08-09 23:49:12 +0600

    10

--- admin_area/insert_categories.php
+++ admin_area/insert_categories.php
@@ -1,5 +1,21 @@
 <?php
-    include(../includes/connect.php);
+include('../includes/connect.php');
+if (isset($_POST['cat_insert'])) {
+    $category_title = $_POST['cat_title'];
+
+    $select_query = "SELECT * FROM category WHERE category_name='$category_title'";
+    $result_search = mysqli_query($connection, $select_query);
+    $number = mysqli_num_rows($result_search);
+    if ($number > 0) {
+        echo "<script>alert('This category is present in database')<script/>";
+    } else {
+        $insert_query = "INSERT into category (category_name) VALUES ('$category_title')";
+        $result_insert = mysqli_query($connection, $insert_query);
+        if ($result_insert) {
+            echo "<script>alert('Inserted Successfully')<script/>";
+        }
+    }
+}
 ?>
 
 <form class="m-5 w-50 mx-auto" action="" method="post">
